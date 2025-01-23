11c166430d9160883a61f867ce69c4f558e69c85
atlantis294 <jfarnault29@gmail.com>
2025-01-23 11:12:41 +0100

    modification slurm

--- run_BSM_opti_slurm.sh
+++ run_BSM_opti_slurm.sh
@@ -8,5 +8,9 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=96
 
+module purge
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/arm/armpl_24.10_gcc/lib
+module load armpl/24.10.0_gcc
+
 # Exécutez votre programme BSM compilé avec les paramètres
-./BSM 1000000 10000
+strace ./BSM 1000000 10000
