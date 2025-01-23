3f2dcbf8bc5aab615fb17de217e4b2b99a2aaca0
atlantis294 <jfarnault29@gmail.com>
2025-01-23 10:35:32 +0100

    modif slurm

--- run_BSM_opti_slurm.sh
+++ run_BSM_opti_slurm.sh
@@ -4,6 +4,9 @@
 #SBATCH --error=BSM_error_%j.log
 #SBATCH --time=01:00:0
 #SBATCH --exclusive
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=96
 
 # Exécutez votre programme BSM compilé avec les paramètres
-./BSM 1000000 1000
+./BSM 1000000 10000
